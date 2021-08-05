ee4e531fdef962465c8a1ed970612ae6036eb64a
JothishR92 <j.eeshwar92@gmail.com>
2021-08-05 07:48:59 +0530

    added directory structure for web development

---
+++ Setup_file_python/setup.py
@@ -0,0 +1,11 @@
+def Install_Requirements():
+    # find windows or linux or mac after install requirement
+    # Here install requirement.txt file only use pip
+    pass
+
+def main():
+    # Call Install_Reuirement function after run the flask file
+    pass
+
+if __name__ == '__main__':
+    main()
